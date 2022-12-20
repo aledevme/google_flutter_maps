c3b9af9d80539b3176a07e31b39006a8f81fdd5c
Alejandro Gonzalez <alexgve7sv@outlook.com>
2022-12-20 16:07:51 +0100

    teletransport to another place and go back current position and also show information in dialog

--- lib/views/mapScreen.dart
+++ lib/views/mapScreen.dart
@@ -12,10 +12,14 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late Completer<GoogleMapController> controller1;
+  late Completer<GoogleMapController> controller1 = Completer();
 
   late LatLng _initialPosition;
   final Set<Marker> _markers = {};
+
+  bool isOpenDialog = false;
+
+  LatLng newYorkPosition = LatLng(40.78343, -73.96625);
 
   late LatLng _lastMapPosition = _initialPosition;
 
@@ -37,14 +41,6 @@ class _MapScreenState extends State<MapScreen> {
 
   MapType _currentMapType = MapType.normal;
 
-  void _onMapTypeButtonPressed() {
-    setState(() {
-      _currentMapType = _currentMapType == MapType.normal
-          ? MapType.satellite
-          : MapType.normal;
-    });
-  }
-
   _onCameraMove(CameraPosition position) {
     _lastMapPosition = position.target;
   }
@@ -65,17 +61,17 @@ class _MapScreenState extends State<MapScreen> {
           mapType: _currentMapType,
           initialCameraPosition: CameraPosition(
             target: _initialPosition,
-            zoom: 14.4746,
+            zoom: 20.4746,
           ),
           onMapCreated: _onMapCreated,
-          zoomGesturesEnabled: true,
+          zoomGesturesEnabled: false,
           onCameraMove: _onCameraMove,
           myLocationEnabled: true,
           compassEnabled: true,
           myLocationButtonEnabled: false,
         ),
         Positioned(
-          bottom: 80,
+          bottom: 120,
           left: 20,
           right: 20,
           child: TextButton(
@@ -86,7 +82,37 @@ class _MapScreenState extends State<MapScreen> {
               'Teleport me to somewhere random',
               style: TextStyle(color: Colors.white),
             ),
-            onPressed: () {},
+            onPressed: () async {
+              GoogleMapController controller = await controller1.future;
+              controller
+                  .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                target: newYorkPosition,
+                zoom: 14,
+              )));
+              setState(() {});
+            },
+          ),
+        ),
+        Positioned(
+          bottom: 80,
+          left: 20,
+          right: 20,
+          child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Color(0xff9A2EEF),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
+            child: Text('Bring me back home',
+                style: TextStyle(color: Colors.white)),
+            onPressed: () async {
+              _getUserLocation();
+              GoogleMapController controller = await controller1.future;
+              controller
+                  .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                target: _initialPosition,
+                zoom: 14,
+              )));
+              setState(() {});
+            },
           ),
         ),
         Positioned(
@@ -97,11 +123,75 @@ class _MapScreenState extends State<MapScreen> {
             style: TextButton.styleFrom(
                 backgroundColor: Color(0xff9A2EEF),
                 padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
-            child: Text('Bring me back home',
+            child: Text(isOpenDialog ? 'Close information' : 'Show information',
                 style: TextStyle(color: Colors.white)),
-            onPressed: () {},
+            onPressed: () async {
+              setState(() {
+                isOpenDialog = !isOpenDialog;
+              });
+            },
           ),
         ),
+        isOpenDialog
+            ? Positioned(
+                bottom: 300,
+                left: 40,
+                right: 40,
+                top: 200,
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  color: Colors.grey.withOpacity(0.6),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Current Location',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Latitude ${_initialPosition.latitude}',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        'Longitude ${_initialPosition.longitude}',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 40),
+                      Text(
+                        'Previous Location',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Latitude ${newYorkPosition.latitude}',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        'Longitude ${newYorkPosition.longitude}',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ))
+            : Container(),
       ],
     ));
   }
